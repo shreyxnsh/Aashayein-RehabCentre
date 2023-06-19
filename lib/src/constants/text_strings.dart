4a70ccd07a38bf38899b3fba88c828abe6f9da1a
shreyxnsh <shreyanshatwork@gmail.com>
2023-06-19 16:27:34 +0530

    welcome screen updated

--- lib/src/constants/text_strings.dart
+++ lib/src/constants/text_strings.dart
@@ -1,5 +1,5 @@
 const String tAppName = "Aashayein.";
 const String tAppTagline = "A place to find healing.";
 
-const String tWelcomeTitle = "Appointments on your fingertips";
-const String tWelcomeSubTitle = "Book appointments at your nearest rehabilitation centre in few clicks";
+const String tWelcomeTitle = "Appointments ?";
+const String tWelcomeSubTitle = "Book appointments at your nearest \n rehabilitation centre in few clicks";
--- lib/src/features/screens/welcome/welcome_screen.dart
+++ lib/src/features/screens/welcome/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:rehabcentre/src/constants/colors.dart';
+import 'package:rehabcentre/src/constants/sizes.dart';
 import 'package:rehabcentre/src/constants/text_strings.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -8,28 +10,61 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Container(
+        padding: const EdgeInsets.all(15),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Image(image: AssetImage('assets/images/welcome-screen-image.png'),),
+            Image(image: AssetImage('assets/images/welcome-screen-image.png'), height: height *0.5,),
             Column(
               children: [
-                Text(tWelcomeTitle),
-                Text(tWelcomeSubTitle),
+                Text(tWelcomeTitle, style: TextStyle(
+                  fontFamily: 'PoppinsBold',
+                  fontSize: 20,
+                ),),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(tWelcomeSubTitle, textAlign: TextAlign.center,
+                style  : TextStyle(
+                  fontFamily: 'PoppinsMedium',
+                  fontSize: 16,
+  
+                ),),
               ],
             ),
             
             Row(
               children: [
-                OutlinedButton(
-                  onPressed: (){}, 
-                  child: Text('Login'),
-                  ),
-                  ElevatedButton(
-                  onPressed: (){}, 
-                  child: Text('Signup'),
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: (){}, 
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(),
+                      foregroundColor: tSecondaryColor,
+                      side: BorderSide(color: tSecondaryColor),
+                      padding: EdgeInsets.symmetric(vertical: tButtonHeight)
+                    ),
+                    child: Text('Login'.toUpperCase()),
+                    ),
+                ),
+                SizedBox(
+                 width: 10.0,
+                ),
+                  Expanded(
+                    child: ElevatedButton(
+                    onPressed: (){}, 
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(),
+                      foregroundColor: tWhiteColor,
+                      backgroundColor: tSecondaryColor,
+                      side: BorderSide(color: tSecondaryColor),
+                      padding: EdgeInsets.symmetric(vertical: tButtonHeight)
+                    ),
+                    child: Text('Signup'.toUpperCase()),
+                    ),
                   ),
                   
               ],
