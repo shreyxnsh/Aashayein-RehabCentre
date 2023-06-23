8ca289c8ecb322162f6f7050e522e877693663d0
shreyxnsh <shreyanshatwork@gmail.com>
2023-06-23 16:38:40 +0530

    profile screen edit option enabled

--- lib/src/features/screens/profile/profile_screen.dart
+++ lib/src/features/screens/profile/profile_screen.dart
@@ -7,6 +7,7 @@ import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:rehabcentre/src/constants/image_strings.dart';
 import 'package:rehabcentre/src/constants/text_strings.dart';
 import 'package:rehabcentre/src/constants/sizes.dart';
+import 'package:rehabcentre/src/features/screens/profile/update_profile_screen.dart';
 
 import '../../../constants/colors.dart';
 
@@ -16,54 +17,80 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true ,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          leading: IconButton(
-            onPressed: (){
-              // go to the previous screen
-              Get.back();
-            },
-            icon: Icon(LineAwesomeIcons.angle_left, color: Colors.black,),
-
+        child: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {
+            // go to the previous screen
+            Get.back();
+          },
+          icon: Icon(
+            LineAwesomeIcons.angle_left,
+            color: Colors.black,
           ),
-          title: Text("Profile", style: TextStyle(
+        ),
+        title: Text(
+          "Profile",
+          style: TextStyle(
             fontFamily: 'PoppinsBold',
             color: Colors.black,
             fontSize: 18,
-          ),),
+          ),
         ),
-        body: SingleChildScrollView(
-          child: Container(
+      ),
+      body: SingleChildScrollView(
+        child: Container(
             padding: EdgeInsets.all(tDefaultSize),
             child: Column(
               children: [
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image(image: AssetImage(tProfileImage))),
+                Stack(
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image(image: AssetImage(tProfileImage))),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: tPrimaryColor,
+                        ),
+                        child: Icon(
+                          LineAwesomeIcons.alternate_pen,
+                          size: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(
                   height: 10,
                 ),
                 Text(
-                   "Shreyansh Jain",
-                   style: TextStyle(
+                  "Shreyansh Jain",
+                  style: TextStyle(
                     fontFamily: 'PoppinsBold',
                     fontSize: 18,
-                   ), 
+                  ),
                 ),
 
-              Text(
-                   "shreyanshatwork@gmail.com",
-                   style: TextStyle(
+                Text(
+                  "shreyanshatwork@gmail.com",
+                  style: TextStyle(
                     fontFamily: 'PoppinsMedium',
                     fontSize: 15,
-                   ), 
+                  ),
                 ),
 
                 SizedBox(
@@ -73,35 +100,27 @@ class ProfileScreen extends StatelessWidget {
                 SizedBox(
                   width: 200,
                   child: ElevatedButton(
-                    
-                    onPressed: (){
-                      
-                    }, 
-                    
-                    
-                    child: Text('Edit Profile', style: TextStyle(
-                      fontFamily: 'PoppinsBold',
-                      color: tDarkColor,
-                    ),
-                    ),
-                  
-                    style: OutlinedButton.styleFrom(
-
+                      onPressed: () {
+                        Get.to(() => UpdateProfileScreen());
+                      },
+                      child: Text(
+                        'Edit Profile',
+                        style: TextStyle(
+                          fontFamily: 'PoppinsBold',
+                          color: tDarkColor,
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
                         shape: StadiumBorder(),
                         foregroundColor: tWhiteColor,
                         backgroundColor: tPrimaryColor,
-                        
                         padding: EdgeInsets.symmetric(vertical: tButtonHeight),
-                
-                    )
-                  ),
+                      )),
                 ),
                 SizedBox(
                   height: 20,
                 ),
-                Divider(
-
-                ),
+                Divider(),
                 SizedBox(
                   height: 10,
                 ),
@@ -109,52 +128,47 @@ class ProfileScreen extends StatelessWidget {
                 ProfileMenuWidget(
                   title: 'Settings',
                   icon: LineAwesomeIcons.cog,
-                  onPress: (){},
+                  onPress: () {},
                 ),
                 ProfileMenuWidget(
                   title: 'Billing Details',
                   icon: LineAwesomeIcons.wallet,
-                  onPress: (){},
+                  onPress: () {},
                 ),
                 ProfileMenuWidget(
                   title: 'User management',
                   icon: LineAwesomeIcons.user_check,
-                  onPress: (){},
+                  onPress: () {},
                 ),
-                
+
                 const Divider(
                   color: Colors.grey,
                 ),
                 const SizedBox(
-                height: 10,
+                  height: 10,
                 ),
                 ProfileMenuWidget(
                   title: 'Information',
                   icon: LineAwesomeIcons.info,
-                  onPress: (){},
+                  onPress: () {},
                 ),
-                
+
                 ProfileMenuWidget(
                   title: 'Logout',
                   textColor: Colors.red,
                   icon: LineAwesomeIcons.alternate_sign_out,
-                  onPress: (){},
+                  onPress: () {},
+                  endIcon: false,
                 ),
-                
-
               ],
-
-            )
-          ),
-        ),
-      )
-      );
+            )),
+      ),
+    ));
   }
 }
 
 class ProfileMenuWidget extends StatelessWidget {
   const ProfileMenuWidget({
-  
     Key? key,
     required this.title,
     required this.icon,
@@ -169,7 +183,6 @@ class ProfileMenuWidget extends StatelessWidget {
   final bool endIcon;
   final Color? textColor;
 
-
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -182,29 +195,30 @@ class ProfileMenuWidget extends StatelessWidget {
           color: tAccentColor.withOpacity(0.1),
         ),
         child: Icon(
-          icon, color: tAccentColor,
+          icon,
+          color: tAccentColor,
         ),
-        
       ),
-      title: Text(title, style: TextStyle(
-        fontFamily: 'PoppinsMedium',
-        fontSize: 16,
-        color: textColor  
+      title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: 'PoppinsMedium', fontSize: 16, color: textColor),
       ),
-      
-      ),
-      trailing: endIcon? Container(
-        width: 30,
-        height: 30,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          color: Colors.grey.withOpacity(0.1),
-        ),
-        child: Icon(
-          LineAwesomeIcons.angle_right, size: 18, color: Colors.grey,
-        ),
-        
-      ) : null,
+      trailing: endIcon
+          ? Container(
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Colors.grey.withOpacity(0.1),
+              ),
+              child: Icon(
+                LineAwesomeIcons.angle_right,
+                size: 18,
+                color: Colors.grey,
+              ),
+            )
+          : null,
     );
   }
 }
