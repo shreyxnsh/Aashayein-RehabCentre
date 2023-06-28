5852f376e9755a031d7650a04584c93ca11f627c
shreyxnsh <shreyanshatwork@gmail.com>
2023-06-28 10:29:39 +0530

    Mail verification completed

---
+++ lib/src/features/controllers/mail_controller.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:rehabcentre/src/repositories/authentication_repository.dart';
+
+class MailController extends GetxController{
+  late Timer _timer;
+  
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    sendVerificationEmail();
+    setTimerForAutoRedirect();
+  }
+  
+  Future<void> sendVerificationEmail() async {
+    try{
+        await AuthenticationRepository.instance.sendEmailVerification();
+    }catch(e){
+      GetSnackBar(
+        title: 'Oh Snap,',
+        message: e.toString(),
+      );
+    }
+    
+  }
+
+  void setTimerForAutoRedirect() {  
+    _timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      FirebaseAuth.instance.currentUser?.reload();
+      final user = FirebaseAuth.instance.currentUser;
+      if(user!.emailVerified){
+        timer.cancel();
+        AuthenticationRepository.instance.setInitialScreen(user);
+      }
+     });
+
+  // }
+
+
+
+  }
+
+  void manuallyCheckVerificationStatus() {
+    FirebaseAuth.instance.currentUser?.reload();
+    final user = FirebaseAuth.instance.currentUser;
+    if(user!.emailVerified){
+        AuthenticationRepository.instance.setInitialScreen(user);
+      }
+  }
+
+}
---
+++ lib/src/features/screens/mail_verification/mail_verification.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:rehabcentre/src/features/controllers/mail_controller.dart';
+import 'package:rehabcentre/src/repositories/authentication_repository.dart';
+
+class MailVerification extends StatelessWidget {
+  const MailVerification({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final controller = Get.put(MailController());
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(padding: EdgeInsets.only(top: 100, left: 20, right: 20, bottom: 40),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(LineAwesomeIcons.envelope_open, size: 100,),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "E-mail verification",
+              style: TextStyle(
+                fontFamily: 'PoppinsBold',
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Subtitle',
+              style: TextStyle(
+                fontFamily: 'PoppinsBold',
+                fontSize: 20,
+              ),
+   
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              child: OutlinedButton(
+                onPressed: () => controller.manuallyCheckVerificationStatus(),
+                child: Text('Continue')
+                
+                ),
+
+
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: () => controller.sendVerificationEmail(), 
+              child: Text('Reset E-mail Verification link')
+              ),
+              
+            TextButton(
+              onPressed: () => AuthenticationRepository.instance.logout(), 
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    LineAwesomeIcons.alternate_long_arrow_left
+                  ),
+                    Text('Back'.tr.toLowerCase())
+                  
+                ],
+              )
+              ),
+              
+          ],
+        ),
+        ),
+        
+      ),
+    );
+  }
+}
