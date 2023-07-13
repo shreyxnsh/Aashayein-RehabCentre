9f47248ddc67df9b5c192c31b54b4a9b4954fbaf
shreyxnsh <shreyanshatwork@gmail.com>
2023-07-13 18:07:11 +0530

    BookAppointment feature completed

--- lib/src/features/screens/appointments/book_appointment_screen.dart
+++ lib/src/features/screens/appointments/book_appointment_screen.dart
@@ -1,10 +1,20 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:rehabcentre/src/features/models/appointment_model.dart';
+import 'package:rehabcentre/src/features/models/user_model.dart';
 
+import '../../../constants/colors.dart';
+import '../../../constants/sizes.dart';
 import '../../models/centre_model.dart';
+import '../profile/profile_screen.dart';
 
 class BookAppointmentScreen extends StatefulWidget {
   final CentreModal centre;
@@ -18,6 +28,8 @@ class BookAppointmentScreen extends StatefulWidget {
 class _BookAppointmentScreenState extends State<BookAppointmentScreen> {
   DateTime? selectedDate;
   TimeOfDay? selectedTime;
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? pickedDate = await showCupertinoModalPopup<DateTime>(
@@ -66,23 +78,21 @@ class _BookAppointmentScreenState extends State<BookAppointmentScreen> {
   }
 
   Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? pickedTime = await showTimePicker(
+    final TimeOfDay? pickedTime = await showModalBottomSheet<TimeOfDay>(
       context: context,
-      initialTime: TimeOfDay.now(),
-      builder: (BuildContext context, Widget? child) {
-        return Theme(
-          data: ThemeData.light().copyWith(
-            colorScheme: ColorScheme.light(
-              primary: Color(0xFFFFE400),
-              onPrimary: Colors.black,
-            ),
-            textSelectionTheme: TextSelectionThemeData(
-              selectionColor: Colors.black,
-              cursorColor: Colors.black,
-              selectionHandleColor: Colors.black,
-            ),
+      builder: (BuildContext context) {
+        return Container(
+          height: 300,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.time,
+            initialDateTime: DateTime.now(),
+            use24hFormat: true,
+            onDateTimeChanged: (DateTime newDateTime) {
+              setState(() {
+                selectedTime = TimeOfDay.fromDateTime(newDateTime);
+              });
+            },
           ),
-          child: child ?? Placeholder(),
         );
       },
     );
@@ -120,7 +130,7 @@ class _BookAppointmentScreenState extends State<BookAppointmentScreen> {
           ),
         ),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -130,17 +140,83 @@ class _BookAppointmentScreenState extends State<BookAppointmentScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Centre Name: ${widget.centre.name}',
+                    '${widget.centre.name}',
                     style: TextStyle(
                       fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                      fontFamily: 'PoppinsBold',
                     ),
                   ),
-                  SizedBox(height: 10),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Category : ',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'PoppinsMedium',
+                        ),
+                      ),
+                      Text(
+                        '${widget.centre.category}',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'PoppinsBold',
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'State : ',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'PoppinsMedium',
+                        ),
+                      ),
+                      Text(
+                        '${widget.centre.state}',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'PoppinsBold',
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Contact : ',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'PoppinsMedium',
+                        ),
+                      ),
+                      Text(
+                        '${widget.centre.contact}',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: 'PoppinsBold',
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
                   Text(
-                    'Centre Address: ${widget.centre.address}',
+                    'Address : ${widget.centre.address}',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 14,
+                      fontFamily: 'PoppinsMedium',
                     ),
                   ),
                   SizedBox(height: 20),
@@ -179,6 +255,29 @@ class _BookAppointmentScreenState extends State<BookAppointmentScreen> {
                       : Container(
                           child: Text('No time selected'),
                         ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      // var details = await FirebaseAuth.instance.currentUser;
+                      bookAppointment();
+                     
+                    },
+                    child: Text(
+                      'Book Appointment',
+                      style: TextStyle(
+                        fontFamily: 'PoppinsBold',
+                        color: tDarkColor,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      shape: StadiumBorder(),
+                      foregroundColor: tWhiteColor,
+                      backgroundColor: tPrimaryColor,
+                      padding: EdgeInsets.symmetric(vertical: tButtonHeight),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -186,5 +285,71 @@ class _BookAppointmentScreenState extends State<BookAppointmentScreen> {
         ),
       ),
     );
+  }
+  
+  Future<void> bookAppointment() async {
+     List DataList =
+                          await AppointmentModal.getSnapshotViaEmail(
+                              ProfileScreen.userData!.email.toString());
+
+                      await UserModal.userFetch();
+                      Map<String, dynamic> temp = {
+                        "username": ProfileScreen.userData!.fullname,
+                        "email": ProfileScreen.userData!.email,
+                        "centername": widget.centre.name,
+                        "category": widget.centre.category,
+                        "state": widget.centre.state,
+                        "contact": widget.centre.contact,
+                        "address": widget.centre.address,
+                        "date": DateFormat('yyyy-MM-dd').format(selectedDate!),
+                        "time": selectedTime!.format(context),
+                        // "time": DateTime.now().toIso8601String(),
+                        "status": "pending"
+                      };
+
+                      DateTime selectedDateTime = DateTime.parse(
+                          "${DateFormat('yyyy-MM-dd').format(selectedDate!)} ${selectedTime!.format(context)}:00");
+
+                      // check if the user has already booked an appointment
+                      for (var i in DataList) {
+                        var date = i["date"];
+                        var time = i["time"];
+
+                        DateTime appointmentTime =
+                            DateTime.parse("$date $time:00");
+                        log(appointmentTime.toIso8601String());
+                        var diff = appointmentTime
+                            .difference(selectedDateTime)
+                            .inHours;
+
+                        log(diff.toString());
+                        // log(diff.inHours.toString());
+                        if ((diff >= 0 && diff < 1)) {
+                          Get.snackbar(
+                            'Appointment Error',
+                            'You have already booked an appointment at this time',
+                            snackPosition: SnackPosition.BOTTOM,
+                            backgroundColor: Colors.red.shade400,
+                            colorText: tWhiteColor,
+                            margin: EdgeInsets.all(16),
+                            borderRadius: 8,
+                            duration: Duration(seconds: 3),
+                          );
+                          return;
+                        }
+                      }
+
+                      await db.collection("Appointments").add(temp);
+                      Get.snackbar(
+                        'Appointment Done',
+                        'Your appointment has been booked',
+                        snackPosition: SnackPosition.BOTTOM,
+                        backgroundColor: Colors.green.shade400,
+                        colorText: Colors.white,
+                        margin: EdgeInsets.all(16),
+                        borderRadius: 8,
+                        duration: Duration(seconds: 3),
+                      );
+
   }
 }
